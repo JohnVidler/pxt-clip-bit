5bbc811e47e747a8f0a73cd83f7aab3eef475334
Joe Finney <joe@comp.lancs.ac.uk>
2023-08-09 16:42:50 +0100

    Introduce cpp function to allow the streaming of log data to the serial port

---
+++ ClipBit.cpp
@@ -0,0 +1,26 @@
+#include "pxt.h"
+#include "ClipBit.h"
+
+namespace ClipBit {
+
+    uint8_t data[129];
+
+    void sendLogViaSerial()
+    {
+        int length = uBit.log.getDataLength(DataFormat::CSV);
+        int blockSize = 128;
+        int i = 0;
+
+        while (length > 0)
+        {
+            memset(data, 0, 129);
+            int l = min (blockSize, length);
+
+            uBit.log.readData(data, i, l, DataFormat::CSV, length);
+            uBit.serial.printf("%s", data);
+
+            length = length - l;
+            i = i + l;
+        }
+    }
+}
---
+++ ClipBit.h
@@ -0,0 +1,11 @@
+/** Definitions for C++ level ClipBit operations go here */
+
+#ifndef CLIP_BIT_H
+#define CLIP_BIT_H
+
+namespace ClipBit 
+{
+    void sendLogViaSerial();
+}
+
+#endif
