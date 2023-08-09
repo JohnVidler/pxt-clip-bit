0b7d327f9809f43949424b3a5cc3f3367b785f9a
Joe Finney <joe@comp.lancs.ac.uk>
2023-08-09 17:12:02 +0100

    Add enums.d.ts

---
+++ enums.d.ts
@@ -0,0 +1,10 @@
+// Auto-generated. Do not edit.
+
+
+    /** Definitions for C++ level ClipBit operations go here */
+
+    declare namespace ClipBit {
+    }
+    
+    // Auto-generated. Do not edit. Really.
+    
