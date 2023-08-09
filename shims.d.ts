1835e87443ebd8a64239f4aad0094d1ecd6574c5
Joe Finney <joe@comp.lancs.ac.uk>
2023-08-09 18:01:16 +0100

    Update shims.d.ts

--- shims.d.ts
+++ shims.d.ts
@@ -1,7 +1,9 @@
 // Auto-generated. Do not edit.
 
-    declare namespace Clipbit {
-    }
-    
-    
-    // Auto-generated. Do not edit. Really.
+
+    /** Definitions for C++ level ClipBit operations go here */
+
+declare namespace ClipBit {
+}
+
+// Auto-generated. Do not edit. Really.
